1a9dab7dd61fd98c1ac1d32bdd036098cb228663
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-10-01 13:27:01 -0500

    added authorization

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "reactportfolioamplif21670ec8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
