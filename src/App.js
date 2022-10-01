154d56ba161a3411b314a8ab5da65e9fc7ef2e80
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-10-01 11:50:08 -0500

    Changed p tag to check for auto update on deployed application

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello! Hogan's Portfolio will be up for v1 soon!
         </p>
         <a
           className="App-link"
