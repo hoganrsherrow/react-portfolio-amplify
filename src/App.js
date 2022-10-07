aa90a683e8e170b89b159f4315c30d38f937102b
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-10-06 20:06:32 -0500

    frontend changes

--- src/App.js
+++ src/App.js
@@ -56,7 +56,8 @@ const App = ({ signOut }) => {
 
   return (
     <View className="App">
-      <Heading level={1}>My Notes App</Heading>
+      <Heading level={1}>Hogan</Heading>
+      <Text>An aspiring developer for hire</Text>
       <View as="form" margin="3rem 0" onSubmit={createNote}>
         <Flex direction="row" justifyContent="center">
           <TextField
